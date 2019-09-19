b758339285adf917b9cc178f5d0855a1db057453
Zee Celest <zeecelest@gmail.com>
2019-09-19 12:12:29 -0400

    added psuedo code

--- zee-domdrills/app.js
+++ zee-domdrills/app.js
@@ -1,2 +1,6 @@
 'use strict';
 //Cat Carousell Drill
+/*when a user selects one of the thumbnail images using the mouse or keyboard, 
+that image should be displayed in the full-size image container at the top. 
+Updating the image should also update its alt attribute. To test keyboard interactivity, 
+you can Tab to one of the thumbnails and press the Enter key.*/
